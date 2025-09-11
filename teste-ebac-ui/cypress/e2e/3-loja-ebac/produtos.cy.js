5f90ee2bd82bfd38ca952dacf79d33e291252db8
Roberto <betoscarpin@gmail.com>
2025-09-11 17:30:18 -0300

    massa de dados em lista

--- teste-ebac-ui/cypress/e2e/3-loja-ebac/produtos.cy.js
+++ teste-ebac-ui/cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -20,10 +20,28 @@ describe('Funcionalidade: Produtos', () => {
     });
 
     it('Deve visitar a pagina do produto', () => {
+        produtosPage.visitarProduto('Autumn Pullie')
+        cy.get('.product_title').should('contain', 'Autumn Pullie' )      
         
     });
 
     it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarProduto('Grayson Crewneck Sweatshirt')
+        produtosPage.addProdutoCarrinho('M', 'Orange', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + ' × “Grayson Crewneck Sweatshirt” foram adicionados no seu carrinho.')
         
+    });
+
+     it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+         cy.fixture('produtos').then(dados => {
+         //let qtd = 7
+         produtosPage.buscarProduto(dados[2].nomeProduto)
+         produtosPage.addProdutoCarrinho(
+            dados[2].tamanho, 
+            dados[2].cor,
+            dados[2].quantidade)
+         cy.get('.woocommerce-message').should('contain', dados[2].nomeProduto)
+         })
     });
 });
